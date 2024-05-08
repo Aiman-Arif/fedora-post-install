06c6163d9d03dc5527686d93ed2ed9d41e2af46b
Muhammad Aiman Arif Bin Baharudin <77523218+Aiman217@users.noreply.github.com>
2024-05-08 18:53:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,17 +25,17 @@ custom_commands=(
   # Improve DNF Speed by updating conf file
   "imp_dnf"
   # Adding RPM Fusion
-  "sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm; sudo dnf groupupdate core; sudo dnf upgrade --refresh"
+  "sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm; sudo dnf groupupdate core; sudo dnf upgrade --refresh"
   # Updating firmware
   "sudo fwupdmgr get-devices; sudo fwupdmgr refresh --force; sudo fwupdmgr get-updates; sudo fwupdmgr update"
   # Removing bloatware
   "sudo dnf remove -y gnome-boxes gnome-contacts gnome-logs gnome-tour mediawriter gnome-abrt firefox"
   # Installing media codecs
-  "sudo dnf groupupdate 'core' 'multimedia' 'sound-and-video' --setopt='install_weak_deps=False' --exclude='PackageKit-gstreamer-plugin' --allowerasing && sync; sudo dnf swap 'ffmpeg-free' 'ffmpeg' --allowerasing; sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg; sudo dnf install lame\* --exclude=lame-devel; sudo dnf group upgrade --with-optional Multimedia"
+  "sudo dnf groupupdate 'core' 'multimedia' 'sound-and-video' --setopt='install_weak_deps=False' --exclude='PackageKit-gstreamer-plugin' --allowerasing && sync; sudo dnf swap 'ffmpeg-free' 'ffmpeg' --allowerasing; sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg; sudo dnf install -y lame\* --exclude=lame-devel; sudo dnf group upgrade -y --with-optional Multimedia"
   # Installing Hoyoverse repo
   "flatpak remote-add --if-not-exists launcher.moe https://gol.launcher.moe/gol.launcher.moe.flatpakrepo"
   # Installing commonly used apps
-  "sudo dnf install gnome-tweaks unzip p7zip p7zip-plugins unrar; flatpak install one.ablaze.floorp io.github.realmazharhussain.GdmSettings com.mattjakeman.ExtensionManager ca.desrt.dconf-editor"
+  "sudo dnf install -y gnome-tweaks unzip p7zip p7zip-plugins unrar; flatpak install -y one.ablaze.floorp io.github.realmazharhussain.GdmSettings com.mattjakeman.ExtensionManager ca.desrt.dconf-editor"
 )
 
 # Define your function
